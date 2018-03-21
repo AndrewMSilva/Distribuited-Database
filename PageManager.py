bb9e9dbacf2ac7787eda0127f0a26eb2e0229216
Gabriel Vassoler <gabiv98@gmail.com>
2018-03-21 18:15:30 -0300

    comentario

--- PageManager.py
+++ PageManager.py
@@ -8,7 +8,7 @@ def Read(cmd):
     if(cmd[0] == 2):
     	DeleteFrom(cmd[1:])
 
-def MetadataPage(name):
+def MetadataPage(name): #verifica se a página já existe
 	try:
 		file = open('__pages__/'+name+'meta.dat', 'rb')
 		file.close()
@@ -17,27 +17,27 @@ def MetadataPage(name):
 		return True
 
 def CreateTable(cmd):
-	if(not MetadataPage(cmd[0])):
+	if(not MetadataPage(cmd[0])): #se já existe n cria denovo e retorna nada
 		print("Tabela "+cmd[0]+" já existente.")
 		return
 	values = []
-	for a in cmd[1:]:
+	for a in cmd[1:]: #pega os atributos do comando
 		v = []
-		if(a[0] == 'int'):
+		if(a[0] == 'int'): #caso o atributo seja inteiro
 			v.append(1)
-			v.append(4)
-			v.append(len(a[1]))
-			v.append(a[1])
-		elif(a[0:4] == 'char'):
-			v.append(2)
+			v.append(4) #tamanho fixo, mas será desconsiderado
+			v.append(len(a[1])) #tamanho do nome do campo
+			v.append(a[1]) #nome do campo
+		elif(a[0:4] == 'char'): #caso seja char
+			v.append(2) 
 			v.append(int((a[0].split('[')[1].split(']'))[0])) #tamanho do char
-			v.append(len(a[1]))
-			v.append(a[1])
-		else:
+			v.append(len(a[1]))#tamanho do nome do campo
+			v.append(a[1])#nome do campo
+		else: #caso seja varchar
 			v.append(3)
 			v.append(int((a[0].split('[')[1].split(']'))[0])) #tamanho do char
-			v.append(len(a[1]))
-			v.append(a[1])
+			v.append(len(a[1])) #tamanho do nome do campo
+			v.append(a[1]) #nome do campo
 		values.append(v)
 	CreateMetaPage(cmd[0],values)
 
@@ -124,7 +124,7 @@ def CreateFrame(pageName, values):
 		print('Error opening '+pageName+'.dat')
 		return False
 
-def InsertInto(cmd):
+def InsertInto(cmd): #ainda não existe eauhhaehauehuea, mas verifica se a tabela existe pra inserir
 	try:
 		file = open('__pages__/'+cmd[0]+'meta.dat', 'rb')
 		return
@@ -134,43 +134,40 @@ def InsertInto(cmd):
 	
 	return
 
-def CreateMetaPage(pageName,attr): # [[type,typeLen,nameLen,name],...]
+def CreateMetaPage(pageName,attr): # [[type,typeLen,nameLen,name],...] | cria a página com os campos da tupla
 	try:
 		file = open('__pages__/'+pageName+'meta.dat', 'wb')
-		pageLen = 8*1024 # 8KB
-		special = 4 # bytes do frame especial
-		headerBytes = 12
-		# criando o header
+		# pega os atributos já verificados e insere um por vez
 		for a in attr:
-			file.write(a[0].to_bytes(1,'little'))
-			file.write(a[1].to_bytes(1,'little'))
-			file.write(a[2].to_bytes(1,'little'))
-			file.write(a[3].encode())
+			file.write(a[0].to_bytes(1,'little')) #tipo do campo
+			file.write(a[1].to_bytes(1,'little')) #tamanho do campo
+			file.write(a[2].to_bytes(1,'little')) #tamanho do nome
+			file.write(a[3].encode()) #pra string
 		# salvando e fechando
 		file.close()
 		return True
 	except IOError:
-		print('Error creating '+pageName+'.dat')
+		print('Error creating '+pageName+'meta.dat') #não deu pra criar a página
 		return False
 
-def getMeta(pageName):
+def getMeta(pageName): #pegar os atributos da tabela
 	try:
 		file = open('__pages__/'+pageName+'meta.dat', 'rb')
 		attr = []
-		a = int.from_bytes(file.read(1), byteorder='little')
+		a = int.from_bytes(file.read(1), byteorder='little') #tipo do primeiro campo, se não existir retorna um vetor vazio
 		while(a):
 			v = []			
 			v.append(a)
-			a = int.from_bytes(file.read(1), byteorder='little')
+			a = int.from_bytes(file.read(1), byteorder='little') #tamanho do campo
 			v.append(a)
-			a = int.from_bytes(file.read(1), byteorder='little')
+			a = int.from_bytes(file.read(1), byteorder='little')#tamanho do nome do campo
 			a = file.read(a).decode()
 			v.append(a)
 			attr.append(v)
-			a = int.from_bytes(file.read(1), byteorder='little')
-		print(attr)
+			a = int.from_bytes(file.read(1), byteorder='little') #nome do campo
+		print(attr) #só teste msm
 		file.close()
 		return attr
 	except IOError:
-		print('Error opening '+pageName+'.dat')
+		print('Error opening '+pageName+'meta.dat') #página não existe
 		return False
