16ad27665206066a24830a88584f2cf97ed8684f
Andrew M. Silva <andrewsaxx@gmail.com>
2019-06-28 14:52:05 -0300

    Adicionando função de include

--- Server/RunServer.py
+++ Server/RunServer.py
@@ -6,7 +6,13 @@ server.Listen()
 
 while server.IsRunning():
 	query = input('> ')
+	try:
+		splited = query.lower().split()
+	except:
+		splited = None
 	if query.lower() == 'close':
 		server.Close()
+	elif splited and splited[0] == 'include' and len(splited) == 2:
+		server.Include(splited[1])
 	else:
 		server.Execute(query)
