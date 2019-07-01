fafce01de7590bfa18fb1a32615cd142ead2efe2
Andrew M. Silva <andrewsaxx@gmail.com>
2019-07-01 14:57:27 -0300

    Eliminando arquivos desnecessários

--- testes/testePequeno.sql
+++
@@ -1,4 +0,0 @@
-create table cliente (nome varchar(257), id int, sobrenome char(30))
-insert into cliente('Antonio hgghfdg', 5, 'que')
-insert into cliente('Joao dhusjaid', 8, 'que')
-select * from cliente
